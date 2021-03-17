756b142a105101bde44d132d594353ee6e5626ae
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-17 13:57:39 +0100

    Creados componentes usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {APP_ROUTING} from './app.routes';
+
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
@@ -8,6 +8,11 @@ import { ClasesComponent } from './components/clases/clases.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
 import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 import { HomeComponent } from './components/home/home.component';
+import {APP_ROUTING} from './app.routes';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,11 @@ import { HomeComponent } from './components/home/home.component';
     ClasesComponent,
     ResaltadoDirective,
     NgSwitchComponent,
-    HomeComponent
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import {HomeComponent} from './components/home/home.component';
 
 const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: '**', pathMatch: 'full', redirectTo:'home'}
+  {path: '**', pathMatch: 'full', redirectTo:'home'},
+  {path: '', pathMatch: 'full', redirectTo:'home'}
 ];
 
 export const APP_ROUTING= RouterModule.forRoot(APP_ROUTES);
