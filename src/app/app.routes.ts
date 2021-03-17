8feef4eff17a01b2d221147b525977d8c808bbe5
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-17 14:27:19 +0100

    Reestructuración rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,13 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {UsuarioComponent} from './components/usuario/usuario.component';
-import {UsuarioNuevoComponent} from './components/usuario/usuario-nuevo.component';
-import {UsuarioEditarComponent} from './components/usuario/usuario-editar.component';
-import {UsuarioDetalleComponent} from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES} from './components/usuario/usuario.routes';
 
 const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'usuario', component: UsuarioComponent},
   {path: 'usuario/:id', component: UsuarioComponent,
-    children:[
-      {path: 'nuevo', component: UsuarioNuevoComponent},
-      {path: 'detalle', component: UsuarioDetalleComponent},
-      {path: 'editar', component: UsuarioEditarComponent},
-      {path: '**', pathMatch: 'full', redirectTo:'nuevo'}
-    ]},
+    children:USUARIO_ROUTES},
   {path: '**', pathMatch: 'full', redirectTo:'home'},
   {path: '', pathMatch: 'full', redirectTo:'home'}
 ];
